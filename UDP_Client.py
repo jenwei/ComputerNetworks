e2b976c686642903e7e56ad8564b5b50321f223b
Jacob Riedel <Jacob.Riedel@students.olin.edu>
2014-02-24 03:12:15 -0500

    Made a working Client and Server Combo.  Its local currently.

--- UDP_Client.py
+++ UDP_Client.py
@@ -7,16 +7,16 @@ class UDP_Client(object):
     """ Computer Networks Chapter 4: Sockets.  UDP Client example. """ 
     
     
-    def __init__(self,Server_Address=("127.0.0.1",5280)):
+    def __init__(self,IP='127.0.0.1', port=7400,Server_Address=("127.0.0.1",5280)):
 
-        socket, AF_INET, SOCK_DGRAM = CN_Sockets.socket, CN_Sockets.AF_INET, CN_Sockets.SOCK_DGRAM
+        socket, AF_INET, SOCK_DGRAM, timeout = CN_Sockets.socket, CN_Sockets.AF_INET, CN_Sockets.SOCK_DGRAM, CN_Sockets.timeout
 
         with socket(AF_INET,SOCK_DGRAM) as sock:
-          
+            sock.bind((IP,port))
+            sock.settimeout(2.0) # 2 second timeout
             
             print ("UDP_Client started for UDP_Server at IP address {} on port {}".format(
-                Server_Address[0],Server_Address[1])
-                   )
+                Server_Address[0],Server_Address[1]))
 
     
             while True:
@@ -32,7 +32,20 @@ class UDP_Client(object):
                 
                 print ("{} bytes sent".format(bytes_sent))
 
-        print ("UDP_Client ended")
+                t=0
+
+                while True:
+                    try:
+                        bytearray_msg, address = sock.recvfrom(1024)
+                        source_IP, source_port = address
+                        str_message =(bytearray_msg.decode("UTF-8"))
+                        print(str_message)
+                        t+=1
+
+                    except timeout:
+                        break
+
+        print("UDP_Client ended")
 
     
 
--- UDP_Server.py
+++ UDP_Server.py
@@ -2,6 +2,7 @@ import CN_Sockets
 import time
 import datetime
 import Users
+
 class UDP_Server(object):
     
 
@@ -13,7 +14,10 @@ class UDP_Server(object):
         with socket(AF_INET, SOCK_DGRAM) as sock:
             sock.bind((IP,port))
             sock.settimeout(2.0) # 2 second timeout
-            
+            Users={}
+            MsgList=[]
+            Times={}
+            MsgCount=0
             print ("UDP Server started on IP Address {}, port {}".format(IP,port))
 
             while True:
@@ -23,20 +27,27 @@ class UDP_Server(object):
                     
                     ts=time.time()
                     st=datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-		    
-                    if address not in Users:
+                    
+                    test=address in Users
+                    if test==False:
                         Users[address]=st
-                    elif address in Users:
+                        MsgList+=['']
+                        Times[st]=MsgCount
+                        MsgCount+=1
+                        bytearray_message = bytearray('Your IP and port have been added to the system!  Congratulations!' ,encoding="UTF-8")
+                        bytes_sent = sock.sendto(bytearray_message, address)
+                    elif test==True:
                         LastMsg=Users[address]
                         print ("\nMessage received from ip address {}, port {}:".format(
                             source_IP,source_port))
                     
-                        str_message =source_IP+(bytearray_msg.decode("UTF-8"))
+                        str_message =[source_IP+': '+(bytearray_msg.decode("UTF-8"))]
+                        print(str_message)
                         
                         Times[st]=MsgCount
                         MsgList+=str_message
-                    
-                        for i in range(Times[LastMsg],Times[st]):
+                        
+                        for i in range(Times[LastMsg]+1,Times[st]+1):
                             bytearray_message = bytearray(MsgList[i],encoding="UTF-8")
                             bytes_sent = sock.sendto(bytearray_message, address)
 
@@ -44,7 +55,6 @@ class UDP_Server(object):
 
                         Users[address]=st
         
-
                 except timeout:
                     print (".",end="",flush=True)
                     continue
