7ce000c62ad679c26c2f1b7f7a32e71dac564399
Jacob Riedel <Jacob.Riedel@students.olin.edu>
2014-02-24 15:42:16 -0500

    Live Server

--- UDP_Client.py
+++ UDP_Client.py
@@ -7,12 +7,11 @@ class UDP_Client(object):
     """ Computer Networks Chapter 4: Sockets.  UDP Client example. """ 
     
     
-    def __init__(self,IP='127.0.0.1', port=7400,Server_Address=("127.0.0.1",5280)):
+    def __init__(self,Server_Address=("192.168.49.127",5280)):
 
         socket, AF_INET, SOCK_DGRAM, timeout = CN_Sockets.socket, CN_Sockets.AF_INET, CN_Sockets.SOCK_DGRAM, CN_Sockets.timeout
 
         with socket(AF_INET,SOCK_DGRAM) as sock:
-            sock.bind((IP,port))
             sock.settimeout(2.0) # 2 second timeout
             
             print ("UDP_Client started for UDP_Server at IP address {} on port {}".format(
@@ -32,15 +31,12 @@ class UDP_Client(object):
                 
                 print ("{} bytes sent".format(bytes_sent))
 
-                t=0
-
                 while True:
                     try:
                         bytearray_msg, address = sock.recvfrom(1024)
                         source_IP, source_port = address
                         str_message =(bytearray_msg.decode("UTF-8"))
                         print(str_message)
-                        t+=1
 
                     except timeout:
                         break
--- UDP_Server.py
+++ UDP_Server.py
@@ -7,7 +7,7 @@ class UDP_Server(object):
     
 
     
-    def __init__(self,IP="127.0.0.1",port=5280):
+    def __init__(self,IP="192.168.49.127",port=5280):
 
         socket, AF_INET, SOCK_DGRAM, timeout = CN_Sockets.socket, CN_Sockets.AF_INET, CN_Sockets.SOCK_DGRAM, CN_Sockets.timeout
         
@@ -38,7 +38,7 @@ class UDP_Server(object):
                         bytes_sent = sock.sendto(bytearray_message, address)
                     elif test==True:
                         LastMsg=Users[address]
-                        print ("\nMessage received from ip address {}, port {}:".format(
+                        print ("\nMessage received from IP address {}, port {}:".format(
                             source_IP,source_port))
                     
                         str_message =[source_IP+': '+(bytearray_msg.decode("UTF-8"))]
