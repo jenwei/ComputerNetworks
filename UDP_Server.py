a18a44e5ca729c7d15762f4e65ac60b053c48b49
Jacob Riedel <Jacob.Riedel@students.olin.edu>
2014-02-24 00:13:36 -0500

    Server Code 'Finished'

--- UDP_Server.py
+++ UDP_Server.py
@@ -1,5 +1,7 @@
 import CN_Sockets
-
+import time
+import datetime
+import Users
 class UDP_Server(object):
     
 
@@ -13,15 +15,34 @@ class UDP_Server(object):
             sock.settimeout(2.0) # 2 second timeout
             
             print ("UDP Server started on IP Address {}, port {}".format(IP,port))
-            
+
             while True:
                 try:
                     bytearray_msg, address = sock.recvfrom(1024)
                     source_IP, source_port = address
                     
-                    print ("\nMessage received from ip address {}, port {}:".format(
-                        source_IP,source_port))
-                    print (bytearray_msg.decode("UTF-8"))
+                    ts=time.time()
+                    st=datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+		    
+                    if address not in Users:
+                        Users[address]=st
+                    elif address in Users:
+                        LastMsg=Users[address]
+                        print ("\nMessage received from ip address {}, port {}:".format(
+                            source_IP,source_port))
+                    
+                        str_message =source_IP+(bytearray_msg.decode("UTF-8"))
+                        
+                        Times[st]=MsgCount
+                        MsgList+=str_message
+                    
+                        for i in range(Times[LastMsg],Times[st]):
+                            bytearray_message = bytearray(MsgList[i],encoding="UTF-8")
+                            bytes_sent = sock.sendto(bytearray_message, address)
+
+                        MsgCount+=1
+
+                        Users[address]=st
         
 
                 except timeout:
